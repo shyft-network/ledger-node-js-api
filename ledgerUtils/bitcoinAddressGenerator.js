17e2d9348b498da80f91a0b9b940675ce7f6a460
Nicolas Morin <nicolas@paycase.com>
2017-10-30 16:01:37 -0400

    btc utils

---
+++ ledgerUtils/bitcoinAddressGenerator.js
@@ -0,0 +1,47 @@
+var numAddressesToGenerate = 2;
+
+
+const ledger = require('../src');
+ledger
+	.comm_node
+	.create_async()
+	.then(function(comm) {
+		console.log(comm.device.getDeviceInfo());
+
+        var btc = new ledger.btc(comm);
+        console.log("btc :: " + JSON.stringify(btc));
+
+    
+        getBitcoinAddress(btc, 0, numAddressesToGenerate);
+
+    })
+	.catch(function(reason) {
+		console.log('An error occured: ', reason);
+	});
+
+var allAddresses = [];
+
+function getBitcoinAddress(btc, addressIndex, totalAddresses) {
+    btc.getWalletPublicKey_async("44'/60'/0'/"+addressIndex).then(
+         function(result) {
+             console.log(result);
+             var bitcoinAddress = result['bitcoinAddress'];
+             console.log('bitcoinAddress');
+             console.log(bitcoinAddress);
+             console.log("generated address " + (addressIndex + 1) + " of " + totalAddresses);
+             allAddresses[addressIndex] = bitcoinAddress;
+             if (addressIndex < totalAddresses - 1) {
+                 getBitcoinAddress(btc, addressIndex + 1, totalAddresses);
+             } else {
+                 finalize();
+             }
+         }).fail(
+         function(error) { console.log("error :: " + error); });
+}
+
+function finalize() {
+    for (var i = 0; i < numAddressesToGenerate; i++) {
+        console.log(allAddresses[i]);
+    }
+    console.log("complete");
+}
